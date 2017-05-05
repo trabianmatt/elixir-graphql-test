0398e11d9db7cab6603d91258b285a87cfe07de6
Matt Dean <matt@trabian.com>
2017-05-05 16:52:17 -0400

    Removed unused code from Phoenix

--- lib/graphql_test/web/endpoint.ex
+++ lib/graphql_test/web/endpoint.ex
@@ -1,16 +1,6 @@
 defmodule GraphQLTest.Web.Endpoint do
   use Phoenix.Endpoint, otp_app: :graphql_test
 
-  socket "/socket", GraphQLTest.Web.UserSocket
-
-  # Serve at "/" the static files from "priv/static" directory.
-  #
-  # You should set gzip to true if you are running phoenix.digest
-  # when deploying your static files in production.
-  plug Plug.Static,
-    at: "/", from: :graphql_test, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
-
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
@@ -36,7 +26,6 @@ defmodule GraphQLTest.Web.Endpoint do
     key: "_graphql_test_key",
     signing_salt: "de0bGhel"
 
-  plug GraphQLTest.Web.Router
 
   @doc """
   Dynamically loads configuration from the system environment
